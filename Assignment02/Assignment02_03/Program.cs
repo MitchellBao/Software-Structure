6879e831d032c4f623305330a825abcd23c529a9
MitchellBao <mitchellbao29@gmail.com>
2025-04-22 16:13:29 +0800

    Add the other 2 Assignments

---
+++ Assignment02/Assignment02_03/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment02_03
+{
+    public class FactorApp
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("please input an interger: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                var primeFactors = GetPrimeFactors(num);
+                Console.Write("the prime factors are: ");
+                foreach (var factor in primeFactors)
+                {
+                    Console.Write($"\t{factor}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"ERROR: {e.Message}");
+            }
+        }
+
+        // 获取素因子（通过除法逐步减少）
+        private static List<int> GetPrimeFactors(int num)
+        {
+            if (num <= 1)
+            {
+                throw new ArgumentException("num must be greater than 1");
+            }
+
+            List<int> primeFactors = new List<int>();
+
+            // 从最小的质数开始除
+            int divisor = 2;
+            while (divisor <= num)
+            {
+                if (num % divisor == 0)
+                {
+                    primeFactors.Add(divisor);
+                    num /= divisor; // 除以质因子
+                }
+                else
+                {
+                    divisor++;
+                }
+            }
+
+            return primeFactors;
+        }
+    }
+}
---
+++ Assignment02/Assignment02_04/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Assignment02_04 
+{ 
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[,] matrix = 
+            {
+                         { 1, 2, 3, 4 },
+                         { 5, 1, 2, 3 },
+                         { 9, 5, 1, 2 }
+            };
+            Console.WriteLine($"The matrix is Toeplitz: {CheckIfToeplitz(matrix)}");
+        }
+
+        // check if it's a Toeplitz matrix
+        public static bool CheckIfToeplitz(int[,] matrix)
+        {
+            if (matrix == null || matrix.GetLength(0) == 0 || matrix.GetLength(1) == 0)
+            {
+                throw new ArgumentException("输入的矩阵不能为空或无效");
+            }
+
+            for (int row = 1; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 1; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] != matrix[row - 1, col - 1])
+                    {
+                        return false; 
+                    }
+                }
+            }
+            return true; 
+        }
+    }
+}
